a7362b26b67d057f10c6418ab7dbd3d99eeef24c
YogiSly <yogisly44@gmail.com>
2021-12-17 17:11:17 +0300

    feature(modal.js):feature in services

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,8 +2,8 @@ import { animate } from "./helpers.js";
 const modal = () => {
   const modalOverlay = document.querySelector(".overlay");
   const buttons = document.querySelectorAll(".fancyboxModal");
-  const btnCallBack = document.querySelectorAll(".call-back");
   const modalCallBack = document.querySelector(".header-modal");
+  const modalServices = document.querySelector(".services-modal");
   const modal = document.querySelectorAll(".fancybox-skin");
 
   buttons.forEach((btns) => {
@@ -11,18 +11,24 @@ const modal = () => {
       modalOverlay.style.display = "block";
       if (e.target.classList.contains("call-back")) {
         modalCallBack.style.display = "block";
+      } else if (e.target.classList.contains("service-btn")) {
+        modalServices.style.display = "block";
       }
     });
   });
 
   modalOverlay.addEventListener("click", () => {
     modalCallBack.style.display = "none";
+    modalServices.style.display = "none";
     modalOverlay.style.display = "none";
   });
   modal.forEach((popup) => {
     popup.addEventListener("click", (e) => {
       if (e.target.classList.contains("header-modal__close")) {
         modalCallBack.style.display = "none";
+        modalOverlay.style.display = "none";
+      } else if (e.target.classList.contains("services-modal__close")) {
+        modalServices.style.display = "none";
         modalOverlay.style.display = "none";
       }
     });
